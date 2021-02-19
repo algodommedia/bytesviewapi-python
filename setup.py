91c667efaf47ce6d1eb19bced5d3db150e5bd1c1
Sanjeev halder <sanjeev@algodommedia.com>
2021-02-19 10:36:52 +0530

    chnage setup version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='bytesviewapi',
-    version='0.1.3',
+    version='0.1.2',
     packages=['bytesviewapi'],
     description='Python library for bytesview client-API Call',
     long_description=long_description,
